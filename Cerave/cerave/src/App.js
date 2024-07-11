6793ece9781c954e357772187751c5086a9624a2
ruchibajra <ruchibajracharya8@gmail.com>
2024-07-11 15:39:48 +0545

    Navbar and npm install

--- Cerave/cerave/src/App.js
+++ Cerave/cerave/src/App.js
@@ -1,15 +1,26 @@
-import './App.css';
-import GreetComponent from './components/props/greetComponent';
-import GreetRamComponent from './components/props/greetRamComponent';
-import React from 'react';
+import "./App.css";
+import AboutComponent from "./components/About/AboutComponent";
+import HomeComponent from "./components/Home/HomeComponent";
+import Navbar from "./components/Navbar/navbar";
+// import GreetComponent from "./components/props/greetComponent";
+// import GreetRamComponent from "./components/props/greetRamComponent";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <GreetRamComponent/>
-    <GreetComponent/>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomeComponent />} />
+            <Route path="/about" element={<AboutComponent />} />
+          </Routes>
+        </div>
+      </Router>
     </>
-     );
+  );
 }
 
 export default App;

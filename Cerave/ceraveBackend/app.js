e9bc5986e4b7801327ce1800127e91f6f186d576
ruchibajra <ruchibajracharya8@gmail.com>
2024-07-18 23:25:39 +0545

    node CRUD operation

--- Cerave/ceraveBackend/app.js
+++ Cerave/ceraveBackend/app.js
@@ -15,7 +15,10 @@ console.log(substraction)
 let division = math.div(10,2);
 console.log(division)
 
+// ---------------------------------------
 // external import from others 
+
+// 1. READ FILE
 fs.readFile('node.txt', 'utf8', (err, data)=>{
     if(err){
         console.error(err);
@@ -24,5 +27,46 @@ fs.readFile('node.txt', 'utf8', (err, data)=>{
     console.log(data);
 })
 
-// HW: write update delete - CRUD 
 
+
+//  2. WRITE FILE : This method will create a new file or overwrite an existing file.
+
+// Data to be written to the file
+const data = 'This is new content for the file.\n';
+
+// Write data to 'node.txt' file
+fs.writeFile('node.txt', data, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to file:', err);
+        return;
+    }
+    console.log('File written successfully.');
+});
+
+
+
+// 3. UPDATE/APPEND FILE : This method adds content to the end of the file without overwriting existing content.
+
+const additionalData = 'This is additional content.\n';
+
+// Append data to 'node.txt' file
+fs.appendFile('node.txt', additionalData, 'utf8', (err) => {
+    if (err) {
+        console.error('Error appending to file:', err);
+        return;
+    }
+    console.log('Content appended successfully.');
+});
+
+
+
+// 4. DELETE FILE
+
+// Delete 'node.txt' file
+fs.unlink('node.txt', (err) => {
+    if (err) {
+        console.error('Error deleting file:', err);
+        return;
+    }
+    console.log('File deleted successfully.');
+});

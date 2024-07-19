2ab05a01e5a34cc85ee4f817be1d3df973f0e8e8
ruchibajra <ruchibajracharya8@gmail.com>
2024-07-19 08:17:00 +0545

    created server from http

--- Cerave/ceraveBackend/app.js
+++ Cerave/ceraveBackend/app.js
@@ -1,72 +1,88 @@
-// import syntax
-const math = require('./math.js');
-const fs = require('fs');
+// // import syntax
+// const math = require('./math.js');
+// const fs = require('fs');
 
-// using custom import
-let sum = math.add(10,2);
-console.log(sum)
+// // using custom import
+// let sum = math.add(10,2);
+// console.log(sum)
 
-let multiplication = math.mul(10,2);
-console.log(multiplication)
+// let multiplication = math.mul(10,2);
+// console.log(multiplication)
 
-let substraction = math.sub(10,2);
-console.log(substraction)
+// let substraction = math.sub(10,2);
+// console.log(substraction)
 
-let division = math.div(10,2);
-console.log(division)
+// let division = math.div(10,2);
+// console.log(division)
 
-// ---------------------------------------
-// external import from others 
+// // ---------------------------------------
+// // external import from others 
 
-// 1. READ FILE
-fs.readFile('node.txt', 'utf8', (err, data)=>{
-    if(err){
-        console.error(err);
-        return
-    }
-    console.log(data);
+// // 1. READ FILE
+// fs.readFile('node.txt', 'utf8', (err, data)=>{
+//     if(err){
+//         console.error(err);
+//         return
+//     }
+//     console.log(data);
+// })
+
+
+
+// //  2. WRITE FILE : This method will create a new file or overwrite an existing file.
+
+// // Data to be written to the file
+// const data = 'This is new content for the file.\n';
+
+// // Write data to 'node.txt' file
+// fs.writeFile('node.txt', data, 'utf8', (err) => {
+//     if (err) {
+//         console.error('Error writing to file:', err);
+//         return;
+//     }
+//     console.log('File written successfully.');
+// });
+
+
+
+// // 3. UPDATE/APPEND FILE : This method adds content to the end of the file without overwriting existing content.
+
+// const additionalData = 'This is additional content.\n';
+
+// // Append data to 'node.txt' file
+// fs.appendFile('node.txt', additionalData, 'utf8', (err) => {
+//     if (err) {
+//         console.error('Error appending to file:', err);
+//         return;
+//     }
+//     console.log('Content appended successfully.');
+// });
+
+
+
+// // 4. DELETE FILE
+
+// // Delete 'node.txt' file
+// fs.unlink('node.txt', (err) => {
+//     if (err) {
+//         console.error('Error deleting file:', err);
+//         return;
+//     }
+//     console.log('File deleted successfully.');
+// });
+
+
+
+// HTTP MODEL - Creating Server!!!
+
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+    res.write('Hello world');
+    res.end();
+
+});
+
+server.listen(3000, () => {
+    console.log('Server is running!');
 })
-
-
-
-//  2. WRITE FILE : This method will create a new file or overwrite an existing file.
-
-// Data to be written to the file
-const data = 'This is new content for the file.\n';
-
-// Write data to 'node.txt' file
-fs.writeFile('node.txt', data, 'utf8', (err) => {
-    if (err) {
-        console.error('Error writing to file:', err);
-        return;
-    }
-    console.log('File written successfully.');
-});
-
-
-
-// 3. UPDATE/APPEND FILE : This method adds content to the end of the file without overwriting existing content.
-
-const additionalData = 'This is additional content.\n';
-
-// Append data to 'node.txt' file
-fs.appendFile('node.txt', additionalData, 'utf8', (err) => {
-    if (err) {
-        console.error('Error appending to file:', err);
-        return;
-    }
-    console.log('Content appended successfully.');
-});
-
-
-
-// 4. DELETE FILE
-
-// Delete 'node.txt' file
-fs.unlink('node.txt', (err) => {
-    if (err) {
-        console.error('Error deleting file:', err);
-        return;
-    }
-    console.log('File deleted successfully.');
-});
